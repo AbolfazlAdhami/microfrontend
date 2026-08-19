f376badf228bf59b2d7178b29418970da14821b6
AbolfazlAdhami <aadhami880@gmail.com>
2026-08-19 10:51:56 +0330

    new: cart element

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,1 +1,16 @@
- 
+import faker from "faker";
+
+const mount = (el) => {
+  const cartText = `<div> You Have ${faker.random.number()} item in your cart</div>`;
+
+  el.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devCart = document.querySelector("#cart-dev");
+  if (devCart) {
+    mount(devCart);
+  }
+}
+
+export { mount };
